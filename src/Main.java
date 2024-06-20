7e0e5e1d51cc45424ef2c4d48e9c5cba9a4b6158
Dumer <dumeti@gmail.com>
2024-06-20 23:23:28 +0500

    test

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+//Задание №1
+        System.out.println(" ");
+        System.out.println("Задание №1");
+        var dog= 8.0;
+        var cat= 3.6;
+        var paper= 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+//Задание №2
+        System.out.println(" ");
+        System.out.println("Задание №2");
+        dog= dog + 4;
+        System.out.println(dog);
+        cat= cat + 4;
+        System.out.println(cat);
+        paper= paper + 4;
+        System.out.println(paper);
+//Задание №3
+        System.out.println(" ");
+        System.out.println("Задание №3");
+        dog= dog + 3.5;
+        cat= cat - 1.6;
+        paper= paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+//Задание №4
+        System.out.println(" ");
+        System.out.println("Задание №4");
+        var friend= 19;
+        System.out.println(friend);
+        friend= friend + 2;
+        System.out.println(friend);
+        friend= friend / 7;
+        System.out.println(friend);
+//Задание №5
+        System.out.println(" ");
+        System.out.println("Задание №5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog= frog * 10;
+        System.out.println(frog);
+        frog= frog / 3.5;
+        System.out.println(frog);
+        frog= frog + 4;
+        System.out.println(frog);
+//Задание №6
+        System.out.println(" ");
+        System.out.println("Задание №6");
+        var boxerWeight1= 78.2;
+        var boxerWeight2= 82.7;
+        var totalWeight= boxerWeight1 + boxerWeight2;
+        var differrenceWeight= boxerWeight2 - boxerWeight1;
+        System.out.println(totalWeight);
+        System.out.println(differrenceWeight);
+//Задание №7
+        System.out.println(" ");
+        System.out.println("Задание №7");
+
+    }
+}
